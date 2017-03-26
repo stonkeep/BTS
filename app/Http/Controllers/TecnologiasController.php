116e321df1aee93530d21289cbd0b505d1e2b347
stonkeep <you@example.com>
2017-03-25 23:42:51 -0300

    created Tecnologias

---
+++ app/Http/Controllers/TecnologiasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tecnologias;
+use Illuminate\Http\Request;
+
+class TecnologiasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tecnologias  $tecnologias
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tecnologias $tecnologias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tecnologias  $tecnologias
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tecnologias $tecnologias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tecnologias  $tecnologias
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tecnologias $tecnologias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tecnologias  $tecnologias
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tecnologias $tecnologias)
+    {
+        //
+    }
+}
