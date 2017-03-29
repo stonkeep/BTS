d64f625e64e7df0dd951feaaf6478c4ea890ab28
Mateus Ezequiel Galasso <das@asda.com>
2017-03-29 15:42:25 -0300

    teste de criação VigenciasPremios

---
+++ app/Exceptions/NotEnoughTicketsException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mateus.galasso
+ * Date: 14/03/2017
+ * Time: 10:12
+ */
+
+namespace App\Exceptions;
+
+class NotEnoughTicketsException extends \RuntimeException
+{
+
+}
