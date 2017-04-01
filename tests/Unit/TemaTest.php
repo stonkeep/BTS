eda4ae6994ad75a3c952da03c2003b6dc09314dd
stonkeep <you@example.com>
2017-04-01 20:41:38 -0300

    temas

---
+++ tests/Unit/TemaTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Temas;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TemaTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     * @test
+     */
+    public function criar_tema()
+    {
+
+        $this->disableExceptionHandling();
+
+        Temas::create([
+            'nome' => 'Alimentação',
+        ]);
+
+        $temas = Temas::firstOrFail();
+
+        $this->assertEquals($temas->nome, 'Alimentação');
+    }
+
+    /** @test */
+    function teste_model_factory()
+    {
+    	$tema = factory(Temas::class)->create();
+
+        $temas = Temas::firstOrFail();
+
+        $this->assertEquals($temas->nome, 'Alimentação');
+    }
+
+    /** @test */
+    function cria_tema_por_post()
+    {
+        //$this->disableExceptionHandling();
+
+    	$this->json('POST', "/temas/create", [
+    	    'nome' => 'Alimentação'
+        ]);
+
+        $temas = Temas::firstOrFail();
+
+
+        $this->assertEquals($temas->nome, 'Alimentação');
+    }
+
+    /** @test */
+    function cria_tema_por_post_recebe_view()
+    {
+        $response = $this->json('POST', "/temas/create", [
+            'nome' => 'Alimentação'
+        ]);
+
+        $response->assertStatus(200);
+
+    }
+}
