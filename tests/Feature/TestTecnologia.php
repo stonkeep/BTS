f9ae955dd17d50f18beeb31991c2456d10a6cd48
Mateus Ezequiel Galasso <das@asda.com>
2017-05-15 16:44:27 -0300

    Mode Post e testes

---
+++ tests/Feature/TestTecnologia.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TestTecnologia extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+    
+    //TODO testar o cadastramento completo (junto com todas as tableas relacionais)
+    //TODO testar o UPDATE completo (junto com todas as tableas relacionais)
+}
---
+++ tests/Unit/TestPost.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TestPost extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    //TODO teste update
+    //TODO teste delete
+    //TODO teste list
+    //TODO teste validações
+}
