d677b98b03e19e9a901648426ecba531b33b1695
Mateus Ezequiel Galasso <das@asda.com>
2017-06-01 17:36:06 -0300

    permissions andando2

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -64,9 +64,10 @@
 
 @section('css')
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/bootstrap-table.css">
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">
+    {{--<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">--}}
 @stop
 
 @section('js')
