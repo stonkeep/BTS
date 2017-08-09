cc28846ece11d73f2cdc5474c3dbb341bb7a76fe
stonkeep <mateusgalasso@yahoo.com.br>
2017-08-09 08:23:53 -0300

    update

--- node_modules/vuetable-2/src/index.js
+++ node_modules/vuetable-2/src/index.js
@@ -2,6 +2,13 @@ import Vuetable from './components/Vuetable.vue'
 import VuetablePagination from './components/VuetablePagination.vue'
 import VuetablePaginationDropDown from './components/VuetablePaginationDropdown.vue'
 import VuetablePaginationInfo from './components/VuetablePaginationInfo.vue'
+import VuetablePaginationMixin from './components/VuetablePaginationMixin.vue'
+import VuetablePaginationInfoMixin from './components/VuetablePaginationInfoMixin.vue'
+import Promise from 'promise-polyfill'
+
+if (!window.Promise) {
+	window.Promise = Promise
+}
 
 function install(Vue){
   Vue.component("vuetable", Vuetable);
@@ -14,6 +21,8 @@ export {
   VuetablePagination,
   VuetablePaginationDropDown,
   VuetablePaginationInfo,
+  VuetablePaginationMixin,
+  VuetablePaginationInfoMixin,
   install
 };
 
